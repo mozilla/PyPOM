6f5de0d6faf43a413bf51d972be7acd700eca147
Dave Hunt <dave.hunt@gmail.com>
2016-11-17 10:11:59 +0000

    Add python 3.5 to classifiers

--- setup.py
+++ setup.py
@@ -34,4 +34,5 @@ setup(name='PyPOM',
           'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4'])
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5'])
