e248698fc10f19ed8115c6257f77d1d84c65c676
Dave Hunt <dave.hunt@gmail.com>
2018-08-01 17:41:28 +0100

    Fix plugin tests

--- tests/test_plugin.py
+++ tests/test_plugin.py
@@ -2,16 +2,18 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from pypom import Region
+from pypom import Region, hookimpl
 
 
 def test_after_wait_for_page_to_load(page):
     log = []
 
     class Plugin:
+        @hookimpl
         def pypom_after_wait_for_page_to_load(self, page):
             log.append(1)
 
+        @hookimpl
         def pypom_after_wait_for_region_to_load(self, region):
             log.append(2)
 
